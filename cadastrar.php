904bac3a106052e520a98b1a88931e0a5d4c8e31
Alexandre Santos <111398880+allesantos@users.noreply.github.com>
2022-12-05 20:46:29 -0300

    Create cadastrar.php
    update

---
+++ cadastrar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+    <div id="corpo-form">
+        <h1>Cadastrar</h1>
+        <form action="">
+            <input type="text" name="nome" placeholder="Nome Completo" maxlength="30">
+            <input type="text" name="telefone" placeholder="Telefone" maxlength="30">
+            <input type="email" name="email" placeholder="Usuário" maxlength="40">
+            <input type="password" name="senha" placeholder="Senha" maxlength="15">
+            <input type="password" name="confSenha" placeholder="Confirmar Senha" maxlength="15">
+            <input type="submit" placeholder="Cadastrar">
+        </form>
+    </div>
+
+</body>
+
+</html>
